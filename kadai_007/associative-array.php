bfd203100cc3dedd58494286027a8cbf921ec789
madoka <mt.mdk.419@gmail.com>
2026-07-06 10:51:51 +0900

    第7章課題アップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charaset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列を作成し変数を代入する
+    $item_data = ['name' =>'onion','price' => 200,'weight' =>160];
+    // 値を出力する
+    print_r($item_data);
+    ?>
+  </p>
+</body>
+
+</html>
